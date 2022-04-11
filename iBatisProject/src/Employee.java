e42205f23ea73b694947dac0626cc95536eadd9d
Ritik <56201149+ritiksaini7247@users.noreply.github.com>
2022-04-11 10:56:34 +0530

    Add files via upload

---
+++ iBatisProject/src/Employee.java
@@ -0,0 +1,55 @@
+public class Employee {
+	private int id;
+	private String first_name;
+	private String last_name;
+	private int salary;
+
+	/* Define constructors for the Employee class. */
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary) {
+		this.first_name = fname;
+		this.last_name = lname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", salary=" + salary
+				+ "]";
+	}
+
+}
